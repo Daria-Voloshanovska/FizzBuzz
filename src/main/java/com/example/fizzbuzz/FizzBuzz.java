bb3227f32e4d08a80ba7de545a9c981c9dfa2737
Daria-Voloshanovska <voloshanovskadaria@gmail.com>
2025-08-28 17:30:06 +0200

    reformat code

--- src/main/java/com/example/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/example/fizzbuzz/FizzBuzz.java
@@ -4,17 +4,19 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class FizzBuzz {
-    private static final Map<Integer,String> rules = new LinkedHashMap<>();
+    private static final Map<Integer, String> rules = new LinkedHashMap<>();
 
     public FizzBuzz addRule(int divisor, String word) {
         rules.put(divisor, word);
         return this;
     }
+
     public void run(int start, int end) {
         for (int i = start; i <= end; i++) {
             System.out.println(getFizzBuzzValue(i));
         }
     }
+
     public String getFizzBuzzValue(int number) {
         StringBuilder output = new StringBuilder();
 
